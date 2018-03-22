9cfc0d2d07613e2cd38e70e272d9a1b975f9987d
ImNezY <36461711+ImNezY@users.noreply.github.com>
2018-03-22 18:37:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,15 +27,6 @@ bot.on('message', (message) => {
         if (message.author.bot) return;
         message.reply('no no no no no, u fagola and now stfu');
     }
-    if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
 });
 // Create an event listener for new guild members
 bot.on('guildMemberAdd', member => {
