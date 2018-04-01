3d28dbfca412d7fc3fc472b8e237322a8815056f
ImNezY <36461711+ImNezY@users.noreply.github.com>
2018-04-01 18:30:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ bot.on('message', (message) => {
         if (message.author.bot) return;
         if (message.author.id === '296740277186723841') return;
         if (message.content.includes('.')) return;
-        if (message.content.includes ('i am')) return; 
-            message.reply('yes you are');
+        message.reply('no u');
+    }
+    if(message.content.includes('i am')) {
+        if (message.author.bot) return;
+        if (message.author.id === '296740277186723841') return;
+        if (message.content.includes('.')) return;
+        if (message.content.includes('sexy')) return;
         message.reply('no u');
     }
     if(message.content == 'szkola') {
